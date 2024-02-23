e151af30a4ac9059b7338b692be8c83d78ebafc8
BaeroBaer <151835387+BaeroBaer@users.noreply.github.com>
2024-02-23 13:40:33 +0100

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -2,9 +2,33 @@
 
 import streamlit as st
 
-#Name des Programms
-st.title('RythmRadar')
+# Haupttitel der Seite
+st.title('RhythmRadar')
 
-#Startseite Big Button
+# Startseite
 st.header('Willkommen bei RhythmRadar')
 st.write('Drücke den Button unten, um mit der Musikerkennung zu starten.')
+
+# Wenn der Button gedrückt wird
+if st.button('Musik erkennen'):
+    ### Hier Funktion zur SmallDickEnergy Musikerkennung aufrufen
+    
+    with st.spinner('Höre zu und erkenne...'):
+    
+        
+        
+        # Wenn erolgreich
+        erkennungserfolg = True
+        if erkennungserfolg:
+            st.success('Erkennung erfolgreich!')
+            st.write('') ### Hier Künstler oder Titel
+            st.write('') ##  Hier Titel oder Künstler
+            
+        else:
+            st.error('Keine Musik erkannt. Bitte versuche es erneut.')
+
+# Erkannte Songs 
+st.header('Meine erkannten Songs')
+## Hier eine Datenbank der erkannten Musik erstellen
+
+
