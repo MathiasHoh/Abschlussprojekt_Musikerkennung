daa050ec972db004b15929502371bd6bb9a900c4
BaeroBaer <kc9667@mci4me.atgit config --global user.name BaeroBaergit config --global user.email kc9667@mci4me.at>
2024-02-23 13:34:48 +0100

    User interface hinzugefuegt

---
+++ user_interface.py
@@ -0,0 +1,10 @@
+
+
+import streamlit as st
+
+#Name des Programms
+st.title('RythmRadar')
+
+#Startseite Big Button
+st.header('Willkommen bei RhythmRadar')
+st.write('Drücke den Button unten, um mit der Musikerkennung zu starten.')
